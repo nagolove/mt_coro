9ddd48939d1fea4c43baa345c89160dbf89cdb4d
nagolove <dekar.own@gmail.com>
2021-10-19 02:41:17 +0300

    base scene code. Not work properly.

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local math = _tl_compat and _tl_compat.math or math; print('hello. I scene from separated thread')
+
+
 require("love")
+require("love_inc").require()
+
 love.filesystem.setRequirePath("?.lua;?/init.lua;scenes/empty/?.lua")
 local i18n = require("i18n")
 
@@ -17,37 +22,28 @@ local i18n = require("i18n")
 
 
 
-local function drawui()
-end
+local event_channel = love.thread.getChannel("event_channel")
+local draw_ready_channel = love.thread.getChannel("draw_ready_channel")
+local graphic_command_channel = love.thread.getChannel("graphic_command_channel")
+local graphic_code_channel = love.thread.getChannel("graphic_code_channel")
 
-local function draw2(graphic_command_channel)
+local accum = 0
 
-end
+local mx, my = 0, 0
 
+local time = love.timer.getTime()
+local dt = 0.
 
+local rendercode = [[
+--local mpos = graphic_command_channel:demand()
+--local ups = graphic_command_channel:demand()
 
+gr.clear(0.5, 0.5, 0.5)
+gr.setColor{0, 0, 0}
+gr.print("TestTest")
+]]
 
-
-
-
-
-
-local function draw()
-   return [[
-    gr.clear(0.5, 0.5, 0.5)
-    gr.setColor{0, 0, 0}
-    gr.print("TestTest")
-    ]]
-end
-
-local function update()
-end
-
-local function keypressed(key)
-   if key == "escape" then
-      love.event.quit()
-   end
-end
+graphic_code_channel:push(rendercode)
 
 local function init()
    i18n.set('en.welcome', 'welcome to this program')
@@ -63,9 +59,55 @@ local function init()
    })
    print("translated", i18n.translate('welcome'))
    print("translated", i18n('welcome'))
+   graphic_command_channel:push(rendercode)
 end
 
-local function quit()
+init()
+
+while true do
+   local events = event_channel:pop()
+   if events then
+      for _, e in ipairs(events) do
+         local evtype = (e)[1]
+         if evtype == "mousemoved" then
+            mx = math.floor((e)[2])
+            my = math.floor((e)[3])
+         elseif evtype == "keypressed" then
+            local key = (e)[2]
+            local scancode = (e)[3]
+            print('keypressed', key, scancode)
+            if scancode == "escape" then
+               love.event.quit()
+            end
+         elseif evtype == "mousepressed" then
+
+
+
+
+
+         end
+      end
+   end
+
+   local nt = love.timer.getTime()
+   dt = nt - time
+   time = nt
+
+
+   if draw_ready_channel:peek() then
+      print('thread drawing')
+
+
+
+
+      local res = draw_ready_channel:pop()
+      if res ~= "ready" then
+         error("Bad message in draw_ready_channel: " .. res)
+      end
+   end
+
+
+   love.timer.sleep(0.001)
 end
 
 
@@ -74,19 +116,6 @@ end
 
 
 
-local function getSceneType()
-   print("i am called")
-   return "multi"
-end
-
-return {
-   init = init,
-   quit = quit,
-   draw = draw,
-
-   getSceneType = getSceneType,
-   update = update,
-   keypressed = keypressed,
 
 
-}
+print('goodbye. I scene from separated thread')
