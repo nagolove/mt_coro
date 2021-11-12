d4438ab852c3fd7638f5cef1dc162b85028f5418
nagolove <dekar.own@gmail.com>
2021-11-12 17:06:43 +0300

    Main cycle move to mainloop() function. More prints.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local math = _tl_compat and _tl_compat.math or math; print('hello. I scene from separated thread')
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local math = _tl_compat and _tl_compat.math or math; local string = _tl_compat and _tl_compat.string or string; print('hello. I scene from separated thread')
 
 
 require("love")
@@ -9,6 +9,8 @@ require('pipeline')
 
 love.filesystem.setRequirePath("?.lua;?/init.lua;scenes/empty/?.lua")
 local i18n = require("i18n")
+local colorize = require('ansicolors2').ansicolors
+local format = string.format
 
 
 
@@ -30,8 +32,7 @@ local event_channel = love.thread.getChannel("event_channel")
 
 local mx, my = 0, 0
 
-local time = love.timer.getTime()
-local dt = 0.
+local last_render
 
 local pipeline = Pipeline.new()
 
@@ -58,9 +59,6 @@ local function init()
    print("translated", i18n('welcome'))
 
    local rendercode = [[
-    --local y = graphic_command_channel:demand()
-    --local x = graphic_command_channel:demand()
-    --local x, y = 100, 100
     local w, h = love.graphics.getDimensions()
     local x, y = math.random() * w, math.random() * h
     love.graphics.setColor{0, 0, 0}
@@ -80,67 +78,78 @@ local function init()
 
 
    pipeline:pushCode('clear', "love.graphics.clear{0.5, 0.5, 0.5}")
+
+   last_render = love.timer.getTime()
+end
+
+local function render()
+   if pipeline:ready() then
+
+
+
+
+
+
+      pipeline:open('text')
+      pipeline:close()
+
+      local x, y = love.mouse.getPosition()
+      local rad = 50
+      pipeline:open('circle_under_mouse')
+      pipeline:push(y)
+      pipeline:push(x)
+      pipeline:push(rad)
+      pipeline:close()
+
+   end
+end
+
+local function mainloop()
+   while true do
+
+      local events = event_channel:pop()
+      if events then
+         for _, e in ipairs(events) do
+            local evtype = (e)[1]
+            if evtype == "mousemoved" then
+               mx = math.floor((e)[2])
+               my = math.floor((e)[3])
+            elseif evtype == "keypressed" then
+               local key = (e)[2]
+               local scancode = (e)[3]
+               print('keypressed', key, scancode)
+               if scancode == "escape" then
+                  love.event.quit()
+               end
+            elseif evtype == "mousepressed" then
+
+
+
+
+
+            end
+         end
+      end
+
+      local nt = love.timer.getTime()
+      local pause = 1. / 60.
+
+      if nt - last_render >= pause then
+         last_render = nt
+         local msg = 'love.timer.getTime() = %f'
+         print(colorize('%{blue}' .. format(msg, love.timer.getTime())))
+         render()
+      end
+
+
+
+
+
+      love.timer.sleep(0.001)
+   end
 end
 
 init()
-
-while true do
-
-   local events = event_channel:pop()
-   if events then
-      for _, e in ipairs(events) do
-         local evtype = (e)[1]
-         if evtype == "mousemoved" then
-            mx = math.floor((e)[2])
-            my = math.floor((e)[3])
-         elseif evtype == "keypressed" then
-            local key = (e)[2]
-            local scancode = (e)[3]
-            print('keypressed', key, scancode)
-            if scancode == "escape" then
-               love.event.quit()
-            end
-         elseif evtype == "mousepressed" then
-
-
-
-
-
-         end
-      end
-   end
-
-   local nt = love.timer.getTime()
-   dt = nt - time
-   time = nt
-
-
-
-
-   pipeline:openAndClose('clear')
-
-   pipeline:open('text')
-   pipeline:close()
-
-   local x, y = love.mouse.getPosition()
-   local rad = 50
-   pipeline:open('circle_under_mouse')
-   pipeline:push(x)
-   pipeline:push(y)
-   pipeline:push(rad)
-   pipeline:close()
-
-
-
-   love.timer.sleep(0.001)
-end
-
-
-
-
-
-
-
-
+mainloop()
 
 print('goodbye. I scene from separated thread')
